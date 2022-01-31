61ed4269bb5f105ce7fa143613ed2a723db12e4b
aoirint <aoirint@users.noreply.github.com>
2022-01-31 14:24:36 +0000

    deploy: 10eab32292dbae0849ccda89128d98e7bfeb8a8d

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-1acdfe26257b39b969ad.js"
+    "url": "webpack-runtime-4f102e504f240dd7ef97.js"
   },
   {
     "url": "framework-b537d5cf0f9fce116299.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "bfc79f86c44a8456276b51c617220bf8"
+    "revision": "8ec804069595020fdd3bb0843feb5775"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-cec99c111be644c8d940.js"
