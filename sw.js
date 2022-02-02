1bac0845b800ef723d0cdeb52b915018053c545d
aoirint <aoirint@users.noreply.github.com>
2022-02-02 23:47:34 +0000

    deploy: 76d03456e018010705cf11b9701a4f633d163fd7

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-b537d5cf0f9fce116299.js"
   },
   {
-    "url": "app-dbe9d3503a726e6b0d63.js"
+    "url": "app-ff37afa0e9cbd96b3025.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1754539ad535fd995cade9db2197559e"
+    "revision": "93394fc32552074b2022519ef4ed21fd"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-cec99c111be644c8d940.js"
@@ -157,7 +157,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-dbe9d3503a726e6b0d63.js`))) {
+  if (!resources || !(await caches.match(`/app-ff37afa0e9cbd96b3025.js`))) {
     return await fetch(event.request)
   }
 
